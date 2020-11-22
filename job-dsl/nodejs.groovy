c00658ca21f2da01eaba4355615363a57471b2f1
Gaurav Balyan <gaurav.balyan24@gmail.com>
2020-11-22 14:00:11 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -9,7 +9,7 @@ job('NodeJS example') {
         scm('H/5 * * * *')
     }
     wrappers {
-        NodeJS Plugin('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('NodeJS Plugin') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
